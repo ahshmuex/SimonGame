da349a7454cbce7ab5b8fc409b6b4f25adf3368b
ahshmuex <64267215+ahshmuex@users.noreply.github.com>
2021-01-04 00:07:28 +0200

    Update game.js

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ $("body").keypress(function() {
  }
 });
 
-$("body").on("tap", function() {
+$("document").on("tap", function() {
   if (!gameStarted) {
     $("#level-title").text("Level " + level);
     nextSequence();
